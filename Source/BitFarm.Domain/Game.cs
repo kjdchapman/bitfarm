8f1a6302f531765d61ac21e8d9022222d6046157
Kyle Chapman <kyle.chapman@wiggle.co.uk>
2013-01-03 23:18:52 +0000

    Add the relevant action spaces for the beginning of the game

--- Source/BitFarm.Domain/Game.cs
+++ Source/BitFarm.Domain/Game.cs
@@ -21,12 +21,19 @@ namespace BitFarm.Domain
 
         public void Start()
         {
-            _actionSpaces = new List<ActionSpace>();
-
-            for (int i = 0; i < 11; i++)
+            _actionSpaces = new List<ActionSpace>
             {
-                _actionSpaces.Add(ActionSpace.Take_One_Grain);
-            }
+                ActionSpace.Build_Rooms_And_Or_Build_Stables,
+                ActionSpace.Build_Stable_And_Or_Bake_Bread,
+                ActionSpace.Day_Labourer,
+                ActionSpace.Fishing,
+                ActionSpace.One_Clay_Stockpile,
+                ActionSpace.One_Reed_Stockpile,
+                ActionSpace.Plow_One_Field_And_Or_Sow,
+                ActionSpace.Starting_Player_And_Storehouse,
+                ActionSpace.Take_One_Grain,
+                ActionSpace.Three_Wood_Stockpile
+            };
         }
 
         public List<ActionSpace> GetActionSpaces()
