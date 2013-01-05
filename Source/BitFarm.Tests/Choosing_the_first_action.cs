3d1385f2f1e44d38c176df177d5994ac62f816ee
Kyle Chapman <kyle.chapman@wiggle.co.uk>
2013-01-05 13:41:36 +0000

    Change test to better reflect move/action/action space distinctions.

---
+++ Source/BitFarm.Tests/Choosing_the_first_action.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BitFarm.Tests
+{
+    [TestFixture]
+    public class Choosing_the_first_action
+    {
+        [Test]
+        public void When_the_game_starts_with_one_player_that_player_will_be_asked_to_choose_an_action_space()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void When_the_game_starts_with_two_players_only_one_player_will_be_asked_to_choose_an_action_space()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
