c5d497da5b2d506b5e159b3f9a3b98d18bcf027b
Kyle Chapman <kyle.chapman@wiggle.co.uk>
2013-01-05 14:01:51 +0000

    Write tests around resource retrieval

--- Source/BitFarm.Tests/Player_resources_at_game_start.cs
+++ Source/BitFarm.Tests/Player_resources_at_game_start.cs
@@ -34,10 +34,33 @@ namespace BitFarm.Tests
         }
 
         [Test]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Trying_to_get_resources_when_the_game_has_not_started_will_throw_an_exception()
+        {
+            var subject = new Game();
+            var stubPlayer = new Mock<IPlayer>().Object;
+            subject.Enrol(stubPlayer);
+
+            subject.GetResourcesFor(stubPlayer);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Trying_to_get_resources_for_a_player_not_in_the_game_will_throw_an_exception()
+        {
+            var subject = new Game();
+            var playerOne = new Mock<IPlayer>().Object;
+            var playerTwo = new Mock<IPlayer>().Object;
+            subject.Enrol(playerOne);
+
+            subject.GetResourcesFor(playerTwo);
+        }
+
+        [Test]
         [Description("One of the players has two food and the starting token, and the other has three food and no starting token")]
         public void At_the_beginning_of_a_two_player_game_the_players_have_the_right_resources()
         {
-            
+            throw new NotImplementedException();    
         }
     }
 }
